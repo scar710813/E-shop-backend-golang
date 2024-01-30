5943bc3d89cb0fc95a6269247ebf6694ad7800a7
scar710813 <unclescar0813@gmail.com>
2024-01-30 15:44:30 -0300

    test: ensure the behaviour to find use by email

--- internal/infra/database/user_database_test.go
+++ internal/infra/database/user_database_test.go
@@ -10,10 +10,7 @@ import (
 )
 
 func TestCreateUser(t *testing.T) {
-	db, err := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
-	if err != nil {
-		t.Fatalf("could not open database: %v", err)
-	}
+	db, _ := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
 	db.AutoMigrate(&entity.User{})
 
 	domainUser, _ := entity.NewUser("Jhomn Doe", "j@j.com", "123456")
@@ -26,6 +23,21 @@ func TestCreateUser(t *testing.T) {
 	findUserError := db.Find(&foundedUser, "id = ?", domainUser.Id).Error
 	assert.Nil(t, findUserError)
 	assert.Equal(t, domainUser.Id, foundedUser.Id)
+	assert.Equal(t, domainUser.Name, foundedUser.Name)
+	assert.Equal(t, domainUser.Email, foundedUser.Email)
+	assert.NotNil(t, foundedUser.Password)
+}
+
+func TestFindByEmail(t *testing.T) {
+	db, _ := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
+	db.AutoMigrate(&entity.User{})
+	domainUser, _ := entity.NewUser("Jhomn Doe", "dj@dj.com", "123456")
+	databaseUser := NewUser(db)
+	databaseUser.Create(domainUser)
+
+	foundedUser, findUserError := databaseUser.FindByEmail(domainUser.Email)
+	assert.Nil(t, findUserError)
+	assert.Equal(t, domainUser.Id.String(), foundedUser.Id.String())
 	assert.Equal(t, domainUser.Name, foundedUser.Name)
 	assert.Equal(t, domainUser.Email, foundedUser.Email)
 	assert.NotNil(t, foundedUser.Password)
